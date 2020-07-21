5e7057af07e38d9556be5201e4d78735336a66a0
go2myself <295945854@qq.com>
2020-07-21 22:30:17 +0800

    项目结构重构

---
+++ build/webpack.config.base.js
@@ -0,0 +1,43 @@
+const path = require('path')
+
+
+const config = {
+  target: 'web',
+  entry: path.join(__dirname, '../client/index.js'),
+  output: {
+    filename: 'bundle.[hash:8].js',
+    path: path.join(__dirname, '../dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        // node_modules里面的js发布的时候已经babel转义过，无需再次处理
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              name: 'resources/[path][name]-[hash:8].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+}
+
+module.exports = config
